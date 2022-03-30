6f269ad4b757a0cf608729eb383d7dae28329169
Eugene Kubesh <codbasters@gmail.com>
2022-03-30 19:27:10 +0300

    update to ng12

--- playground/polyfills.ts
+++ playground/polyfills.ts
@@ -73,7 +73,7 @@ if (typeof (window as any).global === 'undefined') {
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
 
