daaea52f146b82d7e3111d47fa4d8449f63f3f87
Eugene Kubesh <codbasters@gmail.com>
2022-09-29 12:46:15 +0200

    TU-T4055 add "Download" action for gallery view

--- src/app/field-viewer/components/field-view/field-view.component.ts
+++ src/app/field-viewer/components/field-view/field-view.component.ts
@@ -2,10 +2,12 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
+  OnInit,
 } from '@angular/core';
 
 import { Field } from '../../../interfaces/field.interface';
 import { FieldType } from '../../../enums/field-type';
+import { FieldEditorService } from '../../../services/field-editor.service';
 
 
 @Component({
@@ -14,10 +16,30 @@ import { FieldType } from '../../../enums/field-type';
   styleUrls: ['field-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FieldViewComponent {
+export class FieldViewComponent implements OnInit {
 
   @Input() public showLabel = true;
   @Input() public field: Field;
 
   public fieldType = FieldType;
+  public actions = [];
+
+  constructor(
+    private _fieldEditor: FieldEditorService,
+  ) {}
+
+  public ngOnInit() {
+    this._initActions();
+  }
+
+  private _initActions(): void {
+    if (this._fieldEditor.config && this._fieldEditor.config.fileDownload) {
+      this.actions.push({
+        label: 'Download',
+        click: (item) => {
+          this._fieldEditor.config.fileDownload(this.field, item);
+        }
+      });
+    }
+  }
 }
