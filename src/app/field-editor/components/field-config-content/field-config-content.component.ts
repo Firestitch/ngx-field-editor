2a3fe5db8c160f3d17e45cc68256e8f91f16a627
Raymond Gigliotti <ray@firestitch.com>
2023-03-02 14:21:26 -0500

    TU-T4831_Unable_to_upload_an_image_to_Content_Field

--- src/app/field-editor/components/field-config-content/field-config-content.component.ts
+++ src/app/field-editor/components/field-config-content/field-config-content.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
-import { Observable, throwError } from 'rxjs';
-
 import { FsHtmlEditorConfig } from '@firestitch/html-editor';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { FieldComponent } from '../field/field.component';
 import { EditorAction } from '../../../enums';
@@ -10,8 +11,8 @@ import { EditorAction } from '../../../enums';
 
 @Component({
   selector: 'fs-field-config-content',
-  templateUrl: 'field-config-content.component.html',
-  styleUrls: ['field-config-content.component.scss'],
+  templateUrl: './field-config-content.component.html',
+  styleUrls: ['./field-config-content.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FieldConfigContentComponent extends FieldComponent implements OnInit {
@@ -25,8 +26,11 @@ export class FieldConfigContentComponent extends FieldComponent implements OnIni
       image: {
         upload: (file: File): Observable<string> => {
           return this.fieldEditor.action(EditorAction.ImageUpload, this.field, { file })
-        }
-      }
+            .pipe(
+              map((response) => response.url),
+            );
+        },
+      },
     };
   }
 
