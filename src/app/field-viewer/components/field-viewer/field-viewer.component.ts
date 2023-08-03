6db28e1ddf6483b4bda1a21b179b7d2795f2f602
Eugene Kubesh <codbasters@gmail.com>
2023-08-03 19:03:28 +0200

    add showField option for field-viewer

--- src/app/field-viewer/components/field-viewer/field-viewer.component.ts
+++ src/app/field-viewer/components/field-viewer/field-viewer.component.ts
@@ -7,8 +7,10 @@ import {
   Input
 } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+
 import { FieldViewDirective } from '../../directives/field-view/field-view.directive';
-import { Field, FieldViewerConfig } from '../../../interfaces';
+import { Field, FieldOption, FieldViewerConfig } from '../../../interfaces';
 import { FieldType } from '../../../enums';
 
 
@@ -19,18 +21,43 @@ import { FieldType } from '../../../enums';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FieldViewerComponent implements AfterContentInit {
-  
+
   @ContentChildren(FieldViewDirective)
   public fieldViews: QueryList<FieldViewDirective>;
 
   @Input() public hideNotSpecified = false;
 
   public fieldViewTemplateRefs = {};
-  public fields: (Field & { hasValue?: boolean })[] = [];
+  public fields: (Field & { hasValue?: boolean; show?: Observable<boolean> })[] = [];
+
+  private _config: FieldViewerConfig;
 
   @Input('config')
   set setConfig(config: FieldViewerConfig) {
-    this.fields = (config?.fields || [])
+    this._initWithConfig(config);
+  }
+
+  public ngAfterContentInit() {
+    this.fieldViews.forEach((directive: FieldViewDirective) => {
+      this.fieldViewTemplateRefs[directive.type] = directive.templateRef;
+    });
+  }
+
+  public showField(field: Field): Observable<boolean> {
+    if(!this._config.showField) {
+      return of(true);
+    }
+
+    return this._config.showField(field);
+  }
+
+  private _initWithConfig(config: FieldViewerConfig): void {
+    this._initFields(config?.fields);
+    this._config = config;
+  }
+
+  private _initFields(fields: (Field | FieldOption)[]): void {
+    this.fields = (fields || [])
       .map((field) => {
         const value = field.data?.value;
         let hasValue = false;
@@ -39,20 +66,20 @@ export class FieldViewerComponent implements AfterContentInit {
           case FieldType.Heading:
             hasValue = true;
             break;
-            
+
           case FieldType.Address:
             hasValue = Object.keys(value || {}).length !== 0;
-          break;
+            break;
 
           case FieldType.Choice:
           case FieldType.Checkbox:
             hasValue = Object.keys(value?.selected || []).length !== 0;
-          break;
-          
+            break;
+
           case FieldType.Name:
             hasValue = Object.values(value || {})
               .some((name) => !!name);
-          break;
+            break;
 
           default:
             hasValue = value !== null && value !== undefined && value !== '';
@@ -61,17 +88,12 @@ export class FieldViewerComponent implements AfterContentInit {
         return {
           ...field,
           hasValue,
+          show: this.showField(field),
         };
-        
+
       })
       .filter((field) => {
         return !this.hideNotSpecified || field.hasValue;
       });
-  }
-
-  public ngAfterContentInit() {
-    this.fieldViews.forEach((directive: FieldViewDirective) => {
-      this.fieldViewTemplateRefs[directive.type] = directive.templateRef;
-    });
   }
 }
--- src/app/interfaces/field-viewer-config.interface.ts
+++ src/app/interfaces/field-viewer-config.interface.ts
@@ -1,7 +1,10 @@
+import { Observable } from 'rxjs';
+
 import { FieldOption } from './field-option.interface';
 import { Field } from './field.interface';
 
 
 export interface FieldViewerConfig {
-  fields?: (Field | FieldOption)[],
+  fields?: (Field | FieldOption)[];
+  showField?: (field: Field) => Observable<boolean>;
 }
