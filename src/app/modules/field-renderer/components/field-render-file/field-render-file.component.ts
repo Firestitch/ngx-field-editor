76acf6f896ac9c2d9b626ef6ec6990dbb06e8bb6
Raymond Gigliotti <ray@firestitch.com>
2025-03-05 13:18:21 -0500

    TU-T8040 - Forms required file field not working

--- src/app/modules/field-renderer/components/field-render-file/field-render-file.component.ts
+++ src/app/modules/field-renderer/components/field-render-file/field-render-file.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, Optional } from '@angular/core';
+import { ControlContainer, NgForm } from '@angular/forms';
 
-import { of } from 'rxjs';
+import { controlContainerFactory } from '@firestitch/core';
+
 
 import { FieldComponent } from '../field/field.component';
 
@@ -10,28 +12,34 @@ import { FieldComponent } from '../field/field.component';
   templateUrl: './field-render-file.component.html',
   styleUrls: ['./field-render-file.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: ControlContainer,
+      useFactory: controlContainerFactory,
+      deps: [[new Optional(), NgForm]],
+    },
+  ],
 })
 export class FieldRenderFileComponent extends FieldComponent implements OnInit {
 
   public accept;
+  public required = false;
 
   public change(files: any) {
     this.field.data.files = files;
+    this.updateRequired();
   }
 
   public ngOnInit(): void {
     const config = this.field?.configs?.allowedFileTypes || {};
     const allowedTypes = this._getAllowedTypes(config);
     this.accept = allowedTypes.length ? allowedTypes.join(',') : '*';
+    this.updateRequired();
   }
 
-  public validate = () => {
-    if(this.field.configs.required && !this.field.data.files?.length) {
-      return of({ required: true });
-    }
-
-    return of(null);
-  };
+  public updateRequired() {
+    this.required = this.field.configs.required && !this.field.data.files?.length;
+  }
 
   private _getAllowedTypes(allowedTypes) {
     const allowed = [];
